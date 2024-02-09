408e4815a2c1d3829a6c9cf804d960005a5c9e66
NikitaTorbenko <2015nekitciti@gmail.com>
2024-02-09 22:03:08 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@chakra-ui/react";
+
+export const App = () => {
+  return (
+    <div className="">
+      <h2>home</h2>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
+};
