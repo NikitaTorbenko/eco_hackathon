02cf290ed1c2b4cd5842de1200c941e542c09df4
Nikita Lomeiko <lomieiko78@mail.ru>
2024-02-10 08:55:55 +0300

    fix: layout & remove props 'className'

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,10 @@
 import './styles/index.css'
-
-import { Layout } from 'widgets/index'
 import { Routing } from './providers/routing'
 
 export const App = () => {
     return(
         <div className='app'>
-            <Layout className='container'>
-                <Routing/>
-            </Layout>
+            <Routing/>
         </div>
     )
 }
--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -1,26 +1,22 @@
 import { memo } from "react";
+import style from "./Layout.module.scss"
+
 import { Header } from "./header/Header";
 import { Footer } from "./footer/Footer";
 import { Sidebar } from "./sidebar/Sidebar";
 import { Navbar } from "./navbar/Navbar";
 
 interface ILayout {
-    children: React.ReactNode,
-    className?: string
+    children: React.ReactNode
 }
 
-export const Layout: React.FC<ILayout> = memo((props) => {
-    const {
-        children,
-        className
-    } = props
-
+export const Layout: React.FC<ILayout> = memo(({children}) => {
     return(
         <div>
             <Header/>
             <Navbar/>
             <Sidebar/>
-            <div className={className}>
+            <div className={style.container}>
                 {children}
             </div>
             <Footer/>
