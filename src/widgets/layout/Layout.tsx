50b40c2436dfaa524ad1348f0fbd6fdd292f8fac
NikitaTorbenko <2015nekitciti@gmail.com>
2024-02-10 12:26:37 +0300

    fix: footer

--- src/widgets/layout/Layout.tsx
+++ src/widgets/layout/Layout.tsx
@@ -1,27 +1,25 @@
 import { memo } from "react";
-import style from "./Layout.module.scss"
+import style from "./Layout.module.scss";
 
 import { Header } from "./header/Header";
 import { Footer } from "./footer/Footer";
 import { Sidebar } from "./sidebar/Sidebar";
 
 interface ILayout {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export const Layout: React.FC<ILayout> = memo(({children}) => {
-    return(
-        <div>
-            <Header/>
-            <div className={style.content}>
-                <Sidebar>
-                    sidebar
-                </Sidebar>
-                <div className={style.container}>
-                    {children}
-                </div>
-            </div>
-            <Footer/>
+export const Layout: React.FC<ILayout> = memo(({ children }) => {
+  return (
+    <div>
+      <Header />
+      <div className={style.content}>
+        <Sidebar>sidebar</Sidebar>
+        <div className={style.wrap}>
+          <div className={style.container}>{children}</div>
+          <Footer />
         </div>
-    )
-})
+      </div>
+    </div>
+  );
+});
