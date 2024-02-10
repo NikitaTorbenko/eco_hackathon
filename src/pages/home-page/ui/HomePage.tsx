71fcb638271a9fca80cd6c14b4c5b4688b2f8b90
NikitaTorbenko <2015nekitciti@gmail.com>
2024-02-11 01:44:46 +0300

    feat: swiper

--- src/pages/home-page/ui/HomePage.tsx
+++ src/pages/home-page/ui/HomePage.tsx
@@ -1,57 +1,57 @@
-import { Layout } from "widgets/index"
-import style from "./HomePage.module.scss"
-import { IPlacemarkTrash } from "entities/placemark-trash/model/types"
-import { PlacemarkTrash } from "entities/placemark-trash"
-import { Map, YMaps } from "react-yandex-maps"
+import { Layout } from "widgets/index";
+import style from "./HomePage.module.scss";
+import { IPlacemarkTrash } from "entities/placemark-trash/model/types";
+import { PlacemarkTrash } from "entities/placemark-trash";
+import { Map, YMaps } from "react-yandex-maps";
 
 const testApi: IPlacemarkTrash[] = [
-    {
-        nameuser: 'dg',
-        description: '',
-        date: 'DSG',
-        id: 1,
-        images: [],
-        pullutionLevel: 1,
-        сoords: [47.232653, 39.724648]
-    },
-    {
-        nameuser: 'dg',
-        date: 'DSG',
-        description: '',
-        id: 2,
-        images: [],
-        pullutionLevel: 1,
-        сoords: [47.222245, 39.650833]
-    },
-    {
-        nameuser: 'dg',
-        description: '',
-        date: 'DSG',
-        id: 3,
-        images: [],
-        pullutionLevel: 1,
-        сoords: [47.222245, 39.680833]
-    },
-]
+  {
+    creatorId: 0,
+    date: "DSG",
+    id: 1,
+    images: [],
+    pullutionLevel: 1,
+    сoords: [47.232653, 39.724648],
+  },
+  {
+    creatorId: 0,
+    date: "DSG",
+    id: 2,
+    images: [],
+    pullutionLevel: 1,
+    сoords: [47.222245, 39.650833],
+  },
+  {
+    creatorId: 0,
+    date: "DSG",
+    id: 3,
+    images: [],
+    pullutionLevel: 1,
+    сoords: [47.222245, 39.680833],
+  },
+];
 
 const HomePage = () => {
-    return(
-        <Layout isContainer={false}>
-            <div className={style.page}>
-                <YMaps>
-                    <Map 
-                        className={style.map} 
-                        defaultState={{ center: [46.095805, 36.901504], zoom: 8 }}>
-                        {testApi.map(item => 
-                            <PlacemarkTrash 
-                                key={item.id} 
-                                coords={item.сoords} 
-                                indexColor={item.pullutionLevel}/>)}
-                    </Map>
-                </YMaps>
-            </div>
-        </Layout>
-    )
-}
+  return (
+    <Layout isContainer={false}>
+      <div className={style.page}>
+        <YMaps>
+          <Map
+            className={style.map}
+            defaultState={{ center: [46.095805, 36.901504], zoom: 8 }}
+          >
+            {testApi.map((item) => (
+              <PlacemarkTrash
+                key={item.id}
+                coords={item.сoords}
+                indexColor={item.pullutionLevel}
+              />
+            ))}
+          </Map>
+        </YMaps>
+      </div>
+    </Layout>
+  );
+};
 
-export default HomePage
+export default HomePage;
--- src/pages/placemar-page/ui/PlacemarkPage.tsx
+++ src/pages/placemar-page/ui/PlacemarkPage.tsx
@@ -1,72 +1,103 @@
-import { useNavigate, useParams } from "react-router-dom"
-import style from "./PlacemarkPage.module.scss"
-import { useGetPlacemarkByIdQuery } from "entities/placemark-trash"
+import { useNavigate, useParams } from "react-router-dom";
+import style from "./PlacemarkPage.module.scss";
+import { useGetPlacemarkByIdQuery } from "entities/placemark-trash";
 import { FaCalendarAlt } from "react-icons/fa";
 import { FaUser } from "react-icons/fa";
 import { FaTrashRestore } from "react-icons/fa";
 
-import { CircularProgress } from '@chakra-ui/react'
-import { pathRoutes } from "shared/config/route-path"
-import { Layout } from "widgets/index"
+import { CircularProgress } from "@chakra-ui/react";
+import { pathRoutes } from "shared/config/route-path";
+import { Layout } from "widgets/index";
 import { trashLevels } from "shared/data/level-trash";
 import { ReportItem, useGetReportsQuery } from "entities/report";
 
+import { Navigation, Pagination, A11y } from "swiper/modules";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const imgs = ["fwq", "dqwd", "df", "jyht", "gfbv"];
+
 const PlacemarkPage = () => {
-    const {id = 0} = useParams()
-    const navigation = useNavigate()
-    
-    const {data, isLoading} = useGetPlacemarkByIdQuery(Number(id))
-    const {data: dataReport, isLoading: isLoadingReport} = useGetReportsQuery(Number(id));
+  const { id = 0 } = useParams();
+  const navigation = useNavigate();
 
-    if(isLoading)
-        return(
+  const { data, isLoading } = useGetPlacemarkByIdQuery(Number(id));
+  const { data: dataReport, isLoading: isLoadingReport } = useGetReportsQuery(
+    Number(id)
+  );
+
+  if (isLoading)
+    return (
+      <div className={style.wrapper_loader}>
+        <CircularProgress value={80} />
+      </div>
+    );
+
+  // if(data?.length === 0 || !data){
+  //     setTimeout(() => {
+  //         navigation(pathRoutes["not-found"].path)
+  //     }, 200);
+  // }
+
+  return (
+    <Layout>
+      <div className={style.page}>
+        <div className={style.slider}>
+          <Swiper
+            modules={[Navigation, Pagination, A11y]}
+            spaceBetween={50}
+            slidesPerView={3}
+            navigation
+            pagination={{ clickable: true }}
+          >
+            {imgs.map((img, index) => (
+              <SwiperSlide key={index}>
+                <img src={img} alt={index + ""} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div className={style.info}>
+          <span>
+            <FaUser /> Автор: {data?.nameuser || "неизвестно"}
+          </span>
+          <span>
+            <FaTrashRestore />
+            <span>Уровень загрязнения:</span>
+            <div
+              style={{
+                backgroundColor: trashLevels[data?.pullutionLevel || 0].color,
+              }}
+              className={style.color}
+            />
+            <span
+              className={style.level_trash}
+              style={{ color: trashLevels[data?.pullutionLevel || 0].color }}
+            >
+              - {trashLevels[data?.pullutionLevel || 0].name}
+            </span>
+          </span>
+          <span>
+            <FaCalendarAlt /> дата: {data?.date || "неизвестно"}
+          </span>
+        </div>
+        <div className={style.description}>{data?.description}</div>
+        <div className={style.reports}>
+          {isLoadingReport ? (
             <div className={style.wrapper_loader}>
-                 <CircularProgress value={80}/>
+              <CircularProgress value={80} />
             </div>
-        )
-    
-    // if(data?.length === 0 || !data){
-    //     setTimeout(() => {
-    //         navigation(pathRoutes["not-found"].path)
-    //     }, 200);
-    // }
+          ) : (
+            dataReport?.map((item) => <ReportItem key={item.id} {...item} />)
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+};
 
-    return(
-        <Layout>
-            <div className={style.page}>
-                <div className={style.slider}>
-                    
-                </div>
-                <div className={style.info}>
-                    <span><FaUser/> Автор: {data?.nameuser || 'неизвестно'}</span>
-                    <span>
-                        <FaTrashRestore/>
-                        <span>Уровень загрязнения:</span>
-                        <div 
-                            style={{backgroundColor: trashLevels[data?.pullutionLevel || 0].color}} 
-                            className={style.color}/>
-                        <span
-                            className={style.level_trash}
-                            style={{color: trashLevels[data?.pullutionLevel || 0].color}}>
-                                - {trashLevels[data?.pullutionLevel || 0].name}
-                        </span>
-                    </span>
-                    <span><FaCalendarAlt/> дата: {data?.date || 'неизвестно'}</span>
-                </div>
-                <div className={style.description}>
-                    {data?.description}
-                </div>
-                <div className={style.reports}>
-                    {isLoadingReport ?
-                        <div className={style.wrapper_loader}>
-                            <CircularProgress value={80}/>
-                        </div> :
-                    dataReport?.map(item => 
-                            <ReportItem key={item.id} {...item}/>)}
-                </div>
-            </div>
-        </Layout>
-    )
-}
-
-export default PlacemarkPage
+export default PlacemarkPage;
